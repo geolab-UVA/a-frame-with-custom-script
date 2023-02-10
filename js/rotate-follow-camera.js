81c6d2fc55904f5551cb479c380455d5885ddacc
Gennady Uraltsev <gennady.uraltsev@gmail.com>
2023-02-10 16:37:54 -0500

    refactor and simplify

--- js/rotate-follow-camera.js
+++ js/rotate-follow-camera.js
@@ -4,51 +4,17 @@ AFRAME.registerComponent("rotate-follow-camera", {
   init: function () {
     this.rig = document.querySelector("a-scene > a-entity#rig");
     this.camera = this.rig.querySelector("#main-camera");
-    // Register the tickFunction as a throttled tick
-    this.tick = AFRAME.utils.throttleTick(
-      this.tickFunction, //
-      10, // Run at most once every 10ms
-      this
-    );
-
-    /**
-     * Initialize local variables
-     * This is useful
-     */
-    this.cameraPosition = new THREE.Vector3();
   },
 
-  /**
-   * tickFunction that will be wrapped to be throttled.
-   * See the code in the `init` function
-   */
+  tick: function (time, timeDelta) {
+    const cameraPosition = new THREE.Vector3();
 
-  tickFunction: function (time, timeDelta) {
-    /**
-     * Add references to many of the objects in play.
-     * Uncomment the necessary variables
-     */
+    // Store the world position of the camera in cameraPosition
+    const cameraObject3D = this.camera.object3D;
+    cameraObject3D.getWorldPosition(cameraPosition);
 
-    // Rig:
-    // const rig = this.rig;
-    // const rigObject3D = rig.object3D;
-    // const rigPosition = rigObject3D.position;
-    // const rigRotation = rigObject3D.rotation;
-
-    // Camera:
-    const camera = this.camera;
-    const cameraObject3D = camera.object3D;
-    const cameraPosition = this.cameraPosition;
-    cameraObject3D.getWorldPosition(cameraPosition); // Store the world position of the camer in cameraPosition
-
-    // The current entity
-    const entity = this.el;
-    const entityObject3D = entity.object3D;
-    // const elRotation = elObject3D.rotation;
-    // const elPosition = elObject3D.position;
-
-    // # Custom logic
-
+    //Look at camera
+    const entityObject3D = this.el.object3D;
     entityObject3D.lookAt(cameraPosition);
   }
 });
