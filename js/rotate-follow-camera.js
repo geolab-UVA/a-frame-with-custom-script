b654be6518c43ef04c15ca2d87b9ff1a0a0f143c
Gennady Uraltsev <gennady.uraltsev@gmail.com>
2023-02-10 16:54:32 -0500

    debugging and refactoring

--- js/rotate-follow-camera.js
+++ js/rotate-follow-camera.js
@@ -1,9 +1,16 @@
+if (typeof SILENT === "undefined") {
+  SILENT = true;
+}
+
 AFRAME.registerComponent("rotate-follow-camera", {
   schema: {},
 
   init: function () {
     this.rig = document.querySelector("a-scene > a-entity#rig");
     this.camera = this.rig.querySelector("#main-camera");
+
+    SILENT || console.log("Attaching rotate-follow-camera to the entity ");
+    SILENT || console.log(this.el);
   },
 
   tick: function (time, timeDelta) {
