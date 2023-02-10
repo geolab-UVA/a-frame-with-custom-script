6c3e21407f3f6b66c17da99a77a461e43bc94f26
Gennady Uraltsev <gennady.uraltsev@gmail.com>
2023-02-10 16:28:24 -0500

    refactoring

--- js/rotate-follow-camera.js
+++ js/rotate-follow-camera.js
@@ -3,37 +3,52 @@ AFRAME.registerComponent("rotate-follow-camera", {
 
   init: function () {
     this.rig = document.querySelector("a-scene > a-entity#rig");
-    this.camera = this.rig.querySelector("[camera]");
-    this.tick = AFRAME.utils.throttleTick(this.tick, 10, this);
+    this.camera = this.rig.querySelector("#main-camera");
+    // Register the tickFunction as a throttled tick
+    this.tick = AFRAME.utils.throttleTick(
+      this.tickFunction, //
+      10, // Run at most once every 10ms
+      this
+    );
 
-    // Initialize local variables (types)
+    /**
+     * Initialize local variables
+     * This is useful
+     */
     this.cameraPosition = new THREE.Vector3();
   },
 
-  tick: function (time, timeDelta) {
-    // Add references to many of the objects in play
-    // uncomment the necessary variables
+  /**
+   * tickFunction that will be wrapped to be throttled.
+   * See the code in the `init` function
+   */
 
-    // # Rig
+  tickFunction: function (time, timeDelta) {
+    /**
+     * Add references to many of the objects in play.
+     * Uncomment the necessary variables
+     */
+
+    // Rig:
     // const rig = this.rig;
     // const rigObject3D = rig.object3D;
     // const rigPosition = rigObject3D.position;
     // const rigRotation = rigObject3D.rotation;
 
-    // # Camera
+    // Camera:
     const camera = this.camera;
     const cameraObject3D = camera.object3D;
     const cameraPosition = this.cameraPosition;
     cameraObject3D.getWorldPosition(cameraPosition); // Store the world position of the camer in cameraPosition
 
-    // # Element
-    const el = this.el;
-    const elObject3D = el.object3D;
+    // The current entity
+    const entity = this.el;
+    const entityObject3D = entity.object3D;
     // const elRotation = elObject3D.rotation;
     // const elPosition = elObject3D.position;
 
     // # Custom logic
 
-    elObject3D.lookAt(cameraPosition);
+    entityObject3D.lookAt(cameraPosition);
   }
 });
